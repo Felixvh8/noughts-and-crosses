9a94a6ef6a478b6c7b38498977e5148e314c0ba6
Felixvh8 <58196731+Felixvh8@users.noreply.github.com>
2024-04-23 02:53:52 +1200

    adjusted view of console board

--- scripts/board.js
+++ scripts/board.js
@@ -43,11 +43,11 @@ class Board {
   display() {
     let grid = this.toGrid();
     console.log(` 
- ${grid[0]} | ${grid[1]} | ${grid[2]}
----+---+---
- ${grid[3]} | ${grid[4]} | ${grid[5]}
----+---+---
- ${grid[6]} | ${grid[7]} | ${grid[8]}
+       ${grid[0]} | ${grid[1]} | ${grid[2]}
+      ---+---+---
+       ${grid[3]} | ${grid[4]} | ${grid[5]}
+      ---+---+---
+       ${grid[6]} | ${grid[7]} | ${grid[8]}
     `);
   }
 
